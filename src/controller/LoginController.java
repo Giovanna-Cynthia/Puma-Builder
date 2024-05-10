34216e05a9de361892e70332a7b27fc43d9aa9c6
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-05-10 16:41:04 -0300

    Sistema de Login

---
+++ src/controller/LoginController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+
+	@FXML
+	private TextField usuario;
+	
+	@FXML
+	private TextField senha;
+	
+	public Stage primaryStage;
+	
+	public void entrar() {
+		System.out.println("Login realizado com sucesso!");
+		//logica de entrada
+		if(usuario.getText().equals("admin") && senha.getText().equals("admin")) {
+			
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/DashBoard.fxml"));
+				
+				StackPane root = loader.load();
+				
+				//Parent root = loader.load();
+				
+				Scene scene = new Scene(root, 932, 573);
+				
+				Stage currentStage = (Stage) usuario.getScene().getWindow();
+				currentStage.setScene(scene);;
+				currentStage.setTitle("DashBoard");
+				currentStage.show();
+				
+				//primaryStage.setScene(scene);;
+				//primaryStage.setTitle("DashBoard");
+				//primaryStage.show();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			mensagemDeErro();
+		}
+	}
+
+	public void mensagemDeErro() {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("Sua senha esta errada");
+		alert.setContentText("Senha incorreta!");
+		alert.setHeaderText(null);
+		alert.showAndWait();
+	}
+}
---
+++ src/model/Fly.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Fly {
+
+	private int id;
+	
+	private String nome;
+	private String inicioFly;
+	private String fimFly;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getInicioFly() {
+		return inicioFly;
+	}
+	public void setInicioFly(String inicioFly) {
+		this.inicioFly = inicioFly;
+	}
+	public String getFimFly() {
+		return fimFly;
+	}
+	public void setFimFly(String fimFly) {
+		this.fimFly = fimFly;
+	}
+}
