22c3219b5e2150968dbb40fec2211c34c5406ddc
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-05-24 16:43:32 -0300

    Atualizacoes

--- src/controller/ProductsController.java
+++ src/controller/ProductsController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Fly;
 import repository.ProdutosRepository;
 
@@ -24,7 +31,6 @@ public class ProductsController {
 	@FXML
 	private TableColumn<Fly, Integer> cCodigo;
 	
-	
 	@FXML
 	private TextField produto;
 
@@ -36,14 +42,53 @@ public class ProductsController {
 
 	@FXML
 	private TextField codigo;
+	
+	private ObservableList<Fly> data;
 
 	private ProdutosRepository repoProduto;
 	
 	@FXML
 	public void initialize() {
+		//iniciando com o valor default da celula (cell)
+		cProduto.setCellValueFactory(new PropertyValueFactory<>("produto"));
+		cPreco.setCellValueFactory(new PropertyValueFactory<>("preco"));
+		cQuantidade.setCellValueFactory(new PropertyValueFactory<>("quantidade"));
+		cCodigo.setCellValueFactory(new PropertyValueFactory<>("codigo"));
+		
+		//instanciando lista observable
+		data = FXCollections.observableArrayList();
+		 
+		//tableview aceita somente ObservableList
+		tableView.setItems(data);
+		
 		repoProduto = new ProdutosRepository();
+		
+		//Preencher lista
+		carregarDadosSalvos();
 	}
 
+	public void carregarDadosSalvos() {
+		try (BufferedReader br = new BufferedReader(new FileReader("database-pro.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] parts = line.split(";");
+				//preencher o objeto fly
+				if(parts.length >= 5) {
+					Fly fly = new Fly();
+					fly.setId(Integer.parseInt(parts[0]));
+					fly.setProduto(parts[1]);
+					fly.setPreco(Double.parseDouble(parts[2]));
+					fly.setQuantidade(Integer.parseInt(parts[3]));
+					fly.setCodigo(Integer.parseInt(parts[4]));
+					//adicionar no observableList
+					data.add(fly);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	
 	public void adicionar() {
 		Fly fly = new Fly();
 		fly.setProduto(produto.getText());
--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,8 @@ module FlyOutJavaFX {
 	requires javafx.fxml;
 	requires javafx.graphics;
 	
+	opens model to javafx.base;
+	
 	opens application to javafx.graphics, javafx.fxml;
 	opens controller to javafx.fxml;
 }
